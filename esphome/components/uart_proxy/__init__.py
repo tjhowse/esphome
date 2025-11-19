cd0cbd880d84761372ae76cf3a406f2f0f29c685
tjhowse <tjhowse@gmail.com>
2025-11-19 17:50:24 +1000

    .

--- esphome/components/uart_proxy/__init__.py
+++ esphome/components/uart_proxy/__init__.py
@@ -32,6 +32,7 @@ CONFIG_SCHEMA = cv.All(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
     cg.add(var.set_buffer_size(config[CONF_RX_BUFFER_SIZE]))
 
 
